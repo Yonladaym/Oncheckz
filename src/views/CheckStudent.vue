e1b7e2d1aaeb0113b00d4c4e25e9cc48b48fa244
kodchaporn <60070001@it.kmitl.ac.th>
2020-05-04 14:30:06 +0700

    Update CheckStudent.vue

--- src/views/CheckStudent.vue
+++ src/views/CheckStudent.vue
@@ -60,7 +60,7 @@
 
       <div class="down d-flex flex-column justify-center align-self-center" style="width:90%">
         <h4 class="text-center">กรุณาใส่เลข 5 หลัก <br></h4>
-        <br />
+        <br /><br/>
         <input type="text" class="form-control" v-model="code"/>
         <div class="d-flex justify-center">
           <v-btn width="100" height="50" color="#B9CFA0" style="margin:16px" elevation="3" @click="SubmitClass()">
